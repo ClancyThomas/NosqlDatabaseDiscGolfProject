fd30f406795781b4a3b106a3b604045c4e8c9cd0
ClancyThomas <clancyvthomas@gmail.com>
2022-09-21 13:12:52 -0600

    main.java, cloudConnection.java : Added main.java file and added a couple things to cloudConnection.java

--- src/main/cloudConnection.java
+++ src/main/cloudConnection.java
@@ -1,4 +1,32 @@
 package main;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import oracle.nosql.driver.AuthorizationProvider;
+import oracle.nosql.driver.NoSQLHandle;
+import oracle.nosql.driver.NoSQLHandleConfig;
+import oracle.nosql.driver.NoSQLHandleFactory;
+import oracle.nosql.driver.iam.SignatureProvider;
+import oracle.nosql.driver.kv.StoreAccessTokenProvider;
+import oracle.nosql.driver.ops.GetRequest;
+import oracle.nosql.driver.ops.GetResult;
+import oracle.nosql.driver.ops.QueryRequest;
+import oracle.nosql.driver.ops.QueryResult;
+import oracle.nosql.driver.ops.PutRequest;
+import oracle.nosql.driver.ops.PutResult;
+import oracle.nosql.driver.ops.QueryIterableResult;
+import oracle.nosql.driver.ops.Request;
+import oracle.nosql.driver.ops.TableLimits;
+import oracle.nosql.driver.ops.TableRequest;
+import oracle.nosql.driver.values.MapValue;
+
 public class cloudConnection {
+
+    /* Information used for connecting to the database and compartment */
+    private String endpoint = "us-phoenix-1";
+    private String service = "cloud";
+    private String compartment = "practice";
+
+
 }
