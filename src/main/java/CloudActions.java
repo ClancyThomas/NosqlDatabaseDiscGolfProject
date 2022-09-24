0d2808acfb1e79886e8d33866ddedcafcd401818
ClancyThomas <clancyvthomas@gmail.com>
2022-09-23 23:30:33 -0600

    CloudActions.java : Added query function along with a function to print query result to the console and added "year" to addRow() function

--- src/main/java/CloudActions.java
+++ src/main/java/CloudActions.java
@@ -1,27 +1,34 @@
 import oracle.nosql.driver.NoSQLHandle;
 import oracle.nosql.driver.ops.PutRequest;
 import oracle.nosql.driver.ops.PutResult;
+import oracle.nosql.driver.ops.QueryRequest;
+import oracle.nosql.driver.ops.QueryResult;
 import oracle.nosql.driver.values.MapValue;
+
+import java.util.ArrayList;
 
 public class CloudActions {
 
     NoSQLHandle myHandle;
-    private String tableName = "TetonRiverDGC"; // [INSERT] Specify table name here
+    private String tableName = "TetonRiverWeeklyStats"; // [INSERT] Specify table name here
 
     public CloudActions(NoSQLHandle handle) {
         myHandle = handle;
     }
 
-    public void addRow(Integer key, String firstName, String lastName, Integer week,
-                       String course, Integer scoreRelativeToPar, Integer scoreTotal) {
+    public void addRow(String key, String firstName, String lastName, Integer scoreRelativeToPar,
+                       Integer scoreTotal, String course, Integer week, Integer year) {
         MapValue row = new MapValue ()
-                .put("Key", key)
+                .put("id", key)
                 .put("firstName", firstName)
                 .put("lastName", lastName)
-                .put("week", week)
-                .put("course", course)
                 .put("scoreRelativeToPar", scoreRelativeToPar)
-                .put("scoreTotal", scoreTotal);
+                .put("scoreTotal", scoreTotal)
+                .put("course", course)
+                .put("week", week)
+                .put("year", year)
+
+                ;
 
         PutRequest putRequest = new PutRequest()
                 .setValue(row)
@@ -30,5 +37,28 @@ public class CloudActions {
         PutResult putRes = myHandle.put(putRequest);
 
         System.out.println("Put row, result " + putRes);
+    }
+
+    public void query(String sqlQuery, Boolean printToConsole) {
+        QueryRequest queryRequest = new QueryRequest()
+                .setStatement(sqlQuery);
+        ArrayList<MapValue> results = new ArrayList<MapValue>();
+
+        // Use a do while loop because the results may not all come back at once
+        do {
+            QueryResult queryResult = myHandle.query(queryRequest);
+            results.addAll(queryResult.getResults());
+        } while (!queryRequest.isDone());
+        if (printToConsole) {
+            printQueryResult(results);
+        }
+
+    }
+
+    public void printQueryResult(ArrayList<MapValue> results) {
+        System.out.println("Query Result:");
+        for (MapValue res : results) {
+            System.out.println("\t" + res);
+        }
     }
 }
