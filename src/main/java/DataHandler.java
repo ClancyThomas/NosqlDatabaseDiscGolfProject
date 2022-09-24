ae4d2773b5b766bc64418227cafd84dc16743c86
ClancyThomas <clancyvthomas@gmail.com>
2022-09-23 23:31:25 -0600

    DataHandler.java : Added "year" to the database so made the adjustments in data handling

--- src/main/java/DataHandler.java
+++ src/main/java/DataHandler.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
 
 import oracle.nosql.driver.NoSQLHandle;
 
@@ -13,20 +12,20 @@ public class DataHandler {
         databaseWorker = new CloudActions(handle);
     }
 
-    public void uploadCsvFileToDatabase(String fileName, Integer primaryKey, String week, String course) {
+    public void uploadCsvFileToDatabase(String fileName, String letter, String course, String week, String year, String format) {
         String csvLine = "";
 
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("LeagueData2022/" + fileName));
-            Integer count = primaryKey;
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            Integer count = 0;
             reader.readLine(); // This will skip the first line which contains header info
 
             while((csvLine = reader.readLine()) != null) {
                 String[] playerData = csvLine.split(",");
 
                 if(!playerData[1].contains("DNF")) { // Don't use the data if the player did not finish the round (DNF)
-                    String[] player = prepareDataForUpload(playerData, week, course);
-                    uploadToDatabase(player, count);
+                    String[] player = prepareDataForUpload(playerData, course, week, year, format);
+                    uploadToDatabase(player, letter+count);
                     count++;
                 }
             }
@@ -36,8 +35,8 @@ public class DataHandler {
         }
     }
 
-    public String[] prepareDataForUpload(String[] data, String week, String course) {
-        String[] player = new String[6];
+    public String[] prepareDataForUpload(String[] data, String course, String week, String year, String format) {
+        String[] player = new String[8];
 
         String fullName = data[2];
         String cleanFullName = fullName.replaceAll("\"", "");
@@ -48,28 +47,39 @@ public class DataHandler {
             player[1] = splitNames[1];
         else
             player[1] = "";
-        player[2] = week;
-        player[3] = course;
-        player[4] = data[3];
-        player[5] = data[4];
+        player[2] = data[3];
+        player[3] = data[4];
+        player[4] = course;
+        player[5] = week;
+        player[6] = year;
+        player[7] = format;
 
         return player;
     }
 
-    public void uploadToDatabase(String[] player, Integer key) {
+    public void uploadToDatabase(String[] player, String key) {
         int scoreRelativeToPar = 0;
         int scoreTotal = 0;
         int week = 0;
+        int year = 0;
 
         try {
-            week = Integer.parseInt(player[2]);
-            scoreRelativeToPar = Integer.parseInt(player[4]);
-            scoreTotal = Integer.parseInt(player[5]);
+            scoreRelativeToPar = Integer.parseInt(player[2]);
+            scoreTotal = Integer.parseInt(player[3]);
+            week = Integer.parseInt(player[5]);
+            year = Integer.parseInt(player[6]);
+
         }  catch (NumberFormatException e) {
             e.printStackTrace();
         }
 
-        databaseWorker.addRow(key, player[0], player[1], week, player[3], scoreRelativeToPar, scoreTotal);
+        databaseWorker.addRow(key, player[0], player[1], scoreRelativeToPar, scoreTotal, player[4], week, year);
 
+    }
+
+    public void uploadListOfCsvFiles(String[] fileNames) {
+        for(int i = 0; i<fileNames.length; i++) {
+            uploadCsvFileToDatabase(fileNames[i], 1+"a", "Course Name", "1", "2022", "Format");
+        }
     }
 }
